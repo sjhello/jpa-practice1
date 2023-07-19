6e16e79a83fb61c5d1f70d8d14937a99e9572604
sjhello <sjhello235@gmail.com>
2023-07-19 10:48:14 +0900

    feat: 회원 주소 추가

--- src/main/java/jpabook/jpashop/member/Member.java
+++ src/main/java/jpabook/jpashop/member/Member.java
@@ -1,11 +1,12 @@
 package jpabook.jpashop.member;
 
+import jpabook.jpashop.order.Order;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,7 +15,14 @@ public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }

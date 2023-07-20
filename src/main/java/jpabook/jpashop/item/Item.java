b90d499c4ec873f9b35fb4b1850826b2740821ef
sjhello <sjhello235@gmail.com>
2023-07-20 16:32:28 +0900

    feat: 상품 엔티티

--- src/main/java/jpabook/jpashop/item/Item.java
+++ src/main/java/jpabook/jpashop/item/Item.java
@@ -3,15 +3,14 @@ package jpabook.jpashop.item;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
 
     @Id
     @GeneratedValue

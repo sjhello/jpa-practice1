4827e05df30431df58bba156ce9c7239797e0b60
sjhello <sjhello235@gmail.com>
2023-07-24 15:24:01 +0900

    feat & test: 주문 상품 엔티티
     - 주문 상품 생성
     - 주문 상품 제거
     - 주문 상품 금액

--- src/main/java/jpabook/jpashop/order/OrderItem.java
+++ src/main/java/jpabook/jpashop/order/OrderItem.java
@@ -27,4 +27,28 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    /**
+     * 생성 메서드
+     * */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    /**
+     * 비즈니스
+     * */
+    public void cancel() {
+        getItem().addStock(this.count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }

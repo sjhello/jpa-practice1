be7de38f8238b47966c0c6299661e3df71ddc078
sjhello <sjhello235@gmail.com>
2023-07-24 15:16:26 +0900

    refactor: 상품 Fixture 수정

--- src/test/java/jpabook/jpashop/fixture/item/ItemFixture.java
+++ src/test/java/jpabook/jpashop/fixture/item/ItemFixture.java
@@ -1,16 +1,17 @@
 package jpabook.jpashop.fixture.item;
 
 import jpabook.jpashop.item.Album;
+import jpabook.jpashop.item.Book;
 import jpabook.jpashop.item.Item;
 
 public class ItemFixture {
 
     public static Item createItem(String name, int price, int stockQuantity) {
-        Album album = new Album();
-        album.setName(name);
-        album.setPrice(price);
-        album.setStockQuantity(stockQuantity);
+        Item item = new Album();
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
 
-        return album;
+        return item;
     }
 }

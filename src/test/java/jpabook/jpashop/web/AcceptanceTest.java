49032f18b74f42d0ea386b5da8daadf5217b0a02
sjhello <sjhello235@gmail.com>
2023-07-31 14:01:10 +0900

    test: 인수 테스트

---
+++ src/test/java/jpabook/jpashop/web/AcceptanceTest.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.web;
+
+import jpabook.jpashop.util.DatabaseCleanUp;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles(profiles = "test")
+public class AcceptanceTest {
+
+}
